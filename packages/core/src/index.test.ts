78d5fa909687cc74028ab50404b6ea409026d87a
Franklin Chieze <chieze.franklin@gmail.com>
2023-02-28 12:14:50 +0100

    v0.0.2

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,7 +1,7 @@
 import { isID } from '.';
 
 describe('Core', () => {
-    it('should mark a number as ID', () => {
-        expect(isID(1)).toBeTruthy();
-    });
-})
+  it('should mark a number as ID', () => {
+    expect(isID(1)).toBeTruthy();
+  });
+});
