17e48dd1e8cc4c70cd4c3a0d26cb2ca4a4180e56
Franklin Chieze <chieze.franklin@gmail.com>
2024-04-22 19:18:47 +0200

    v0.1.5

--- packages/core/src/types/queries/number.ts
+++ packages/core/src/types/queries/number.ts
@@ -2,8 +2,9 @@ export type NumberFields<T> = {
   [key in keyof T]: T[key] extends number ? NumberOps<T> : never;
 };
 
-export type NumberOps<T> = GreaterThanNumberOp<T> | LessThanNumberOp<T>;
+export type NumberOps<T> = ArrayNumberOp<T> | GreaterThanNumberOp<T> | LessThanNumberOp<T>;
 
+export type ArrayNumberOp<T> = { $in: number[] };
 export type GreaterThanNumberOp<T> = { $gt: number };
 export type LessThanNumberOp<T> = { $lt: number };
 // $eq, $ne, $gt, $gte, $lt, $lte
--- packages/core/src/types/queries/string.ts
+++ packages/core/src/types/queries/string.ts
@@ -2,8 +2,8 @@ export type StringFields<T> = {
   [key in keyof T]: T[key] extends string ? StringOps<T> : never;
 };
 
-export type StringOps<T> = GreaterThanStringOp<T> | LessThanStringOp<T>;
+export type StringOps<T> = ArrayStringOp<T> | GreaterThanStringOp<T> | LessThanStringOp<T>;
 
+export type ArrayStringOp<T> = { $in: string[] };
 export type GreaterThanStringOp<T> = { $gt: string };
 export type LessThanStringOp<T> = { $lt: string };
-export type ArrayStringOp<T> = { $in: string[] };
