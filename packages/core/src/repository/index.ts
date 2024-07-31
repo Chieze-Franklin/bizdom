df94814a180d21694785daa1d92b937d2935f7da
Franklin Chieze <chieze.franklin@gmail.com>
2024-07-31 23:57:22 +0200

    v0.1.8

--- packages/core/src/repository/index.ts
+++ packages/core/src/repository/index.ts
@@ -1,71 +1,91 @@
-import { FilterQuery, ID, OperationResult, Persisted, SaveInput, SortQuery, UpdateInput } from '../types';
+import {
+  ComplexFilterQuery,
+  FilterQuery,
+  ID,
+  IncludeQuery,
+  OperationResult,
+  Persisted,
+  SaveInput,
+  SelectQuery,
+  SortQuery,
+  UpdateInput,
+} from '../types';
 
 export interface IModelEntityMapper<T, Entity = any> {
   toModel(entity: Entity): T;
   toEntity(model: T): Entity;
 }
 
-export interface IQueryBuilder<T> {
-  and(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  beginGroup(): IQueryBuilder<T>;
-  build(): any;
-  buildToJSON(): any;
-  endGroup(): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  limit(limit: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  or(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  skip(skip: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  sort(query: SortQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  where(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+export interface IQueryOptions<T> {
+  include?: IncludeQuery<T>;
+  limit?: number;
+  select?: SelectQuery<T>;
+  skip?: number;
+  sort?: SortQuery<T>;
+  where?: ComplexFilterQuery<T>;
 }
 
-export abstract class QueryBuilder<T> implements IQueryBuilder<T> {
-  abstract and(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract beginGroup(): IQueryBuilder<T>;
-  abstract build(): any;
-  buildToJSON() {
-    return JSON.stringify(this.build());
-  }
-  abstract endGroup(): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract limit(limit: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract or(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract skip(skip: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract sort(query: SortQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-  abstract where(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
-}
+// export interface IQueryBuilder<T> {
+//   and(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   beginGroup(): IQueryBuilder<T>;
+//   build(): any;
+//   buildToJSON(): any;
+//   endGroup(): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   limit(limit: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   or(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   skip(skip: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   sort(query: SortQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   where(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+// }
+
+// export abstract class QueryBuilder<T> implements IQueryBuilder<T> {
+//   abstract and(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract beginGroup(): IQueryBuilder<T>;
+//   abstract build(): any;
+//   buildToJSON() {
+//     return JSON.stringify(this.build());
+//   }
+//   abstract endGroup(): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract limit(limit: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract or(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract skip(skip: number): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract sort(query: SortQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+//   abstract where(query: FilterQuery<T>): IQueryBuilder<T>; // Omit<IQueryBuilder<T>, 'where'>;
+// }
 
 export interface IRepository<T> {
-  // aggregate(params?: IQueryBuilder<T>): Promise<Persisted<T>[]>;
-  count(params?: IQueryBuilder<T>): Promise<number>;
+  // aggregate(options?: IQueryBuilder<T>): Promise<Persisted<T>[]>;
+  count(options?: IQueryOptions<T>): Promise<number>;
   delete(id: ID): Promise<Persisted<T>>;
-  deleteMany(params: IQueryBuilder<T>): Promise<OperationResult>;
-  // distinct(params: IQueryBuilder<T>): Promise<Persisted<T>[]>;
-  // drop(name: string, params: IQueryBuilder<T>): Promise<number>;
-  exists(params: IQueryBuilder<T>): Promise<boolean>;
-  find(id: ID): Promise<Persisted<T> | null>;
+  deleteMany(options: IQueryOptions<T>): Promise<OperationResult>;
+  // distinct(options: IQueryOptions<T>): Promise<Persisted<T>[]>;
+  // drop(name: string, options: IQueryOptions<T>): Promise<number>;
+  exists(options: IQueryOptions<T>): Promise<boolean>;
+  find(id: ID, options?: IQueryOptions<T>): Promise<Persisted<T> | null>;
   findAndDelete(id: ID): Promise<Persisted<T> | null>;
   findAndUpdate(id: ID, data: UpdateInput<T>): Promise<Persisted<T> | null>;
-  get(id: ID): Promise<Persisted<T>>;
-  getMany(params?: IQueryBuilder<T>): Promise<Persisted<T>[]>;
-  // group(name: string, params?: IQueryBuilder<T>): Promise<number>;
+  get(id: ID, options?: IQueryOptions<T>): Promise<Persisted<T>>;
+  getMany(options?: IQueryOptions<T>): Promise<Persisted<T>[]>;
+  // group(name: string, options?: IQueryOptions<T>): Promise<number>;
   save(data: SaveInput<T>): Promise<Persisted<T>>;
   update(data: UpdateInput<T>): Promise<Persisted<T>>;
-  updateMany(params: IQueryBuilder<T>, data: UpdateInput<T>): Promise<OperationResult>;
+  updateMany(options: IQueryOptions<T>, data: UpdateInput<T>): Promise<OperationResult>;
 }
 
 export class NullRepository<T> implements IRepository<T> {
-  count(params?: IQueryBuilder<T>): Promise<number> {
+  count(options?: IQueryOptions<T>): Promise<number> {
     throw new Error('Method not implemented.');
   }
   delete(id: ID): Promise<Persisted<T>> {
     throw new Error('Method not implemented.');
   }
-  deleteMany(params: IQueryBuilder<T>): Promise<OperationResult> {
+  deleteMany(options: IQueryOptions<T>): Promise<OperationResult> {
     throw new Error('Method not implemented.');
   }
-  exists(params: IQueryBuilder<T>): Promise<boolean> {
+  exists(options: IQueryOptions<T>): Promise<boolean> {
     throw new Error('Method not implemented.');
   }
-  find(id: ID): Promise<Persisted<T> | null> {
+  find(id: ID, options?: IQueryOptions<T>): Promise<Persisted<T> | null> {
     throw new Error('Method not implemented.');
   }
   findAndDelete(id: ID): Promise<Persisted<T> | null> {
@@ -74,10 +94,10 @@ export class NullRepository<T> implements IRepository<T> {
   findAndUpdate(id: ID, data: UpdateInput<T>): Promise<Persisted<T> | null> {
     throw new Error('Method not implemented.');
   }
-  get(id: ID): Promise<Persisted<T>> {
+  get(id: ID, options?: IQueryOptions<T>): Promise<Persisted<T>> {
     throw new Error('Method not implemented.');
   }
-  getMany(params?: IQueryBuilder<T>): Promise<Persisted<T>[]> {
+  getMany(options?: IQueryOptions<T>): Promise<Persisted<T>[]> {
     throw new Error('Method not implemented.');
   }
   save(data: SaveInput<T>): Promise<Persisted<T>> {
@@ -86,7 +106,7 @@ export class NullRepository<T> implements IRepository<T> {
   update(data: UpdateInput<T>): Promise<Persisted<T>> {
     throw new Error('Method not implemented.');
   }
-  updateMany(params: IQueryBuilder<T>, data: UpdateInput<T>): Promise<OperationResult> {
+  updateMany(options: IQueryOptions<T>, data: UpdateInput<T>): Promise<OperationResult> {
     throw new Error('Method not implemented.');
   }
 }
--- packages/core/src/types/queries/index.ts
+++ packages/core/src/types/queries/index.ts
@@ -5,7 +5,21 @@ import { StringFields } from './string';
 
 export type FilterQuery<T> = NumberFields<T> | SimpleFilterQuery<T> | StringFields<T>;
 
+export type ComplexFilterQuery<T> =
+  | FilterQuery<T>
+  | {
+      $and?: FilterQuery<T>[] | ComplexFilterQuery<T>[];
+      $or?: FilterQuery<T>[] | ComplexFilterQuery<T>[];
+    };
+
 export type IncludeQuery<T> = {
+  [key in keyof T]?: {
+    include?: IncludeQuery<T[key]>;
+    select?: SelectQuery<T[key]>;
+  };
+};
+
+export type SelectQuery<T> = {
   [key in keyof T]?: 1 | -1;
 };
 
